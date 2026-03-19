6126f832e92f35d7b8ed309dfd5f89411bed17b1
asmaylalrdy <asmaylalrdy744@gmail.com>
2026-03-19 10:50:13 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'rady-wars-v1';
+const assets = [
+    './',
+    './index.html',
+    './style.css',
+    './game.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
